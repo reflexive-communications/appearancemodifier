cc74bbb316996a1264ad1393a69b86bd691bf737
akosgarai <szlem01@gmail.com>
2021-07-30 11:44:45 +0200

    Generate Petition entity for the modifier
    Output of the 'civix generate:entity --api-version=3,4 AppearancemodifierPetition' command.

---
+++ CRM/Appearancemodifier/BAO/AppearancemodifierPetition.php
@@ -0,0 +1,26 @@
+<?php
+use CRM_Appearancemodifier_ExtensionUtil as E;
+
+class CRM_Appearancemodifier_BAO_AppearancemodifierPetition extends CRM_Appearancemodifier_DAO_AppearancemodifierPetition {
+
+  /**
+   * Create a new AppearancemodifierPetition based on array-data
+   *
+   * @param array $params key-value pairs
+   * @return CRM_Appearancemodifier_DAO_AppearancemodifierPetition|NULL
+   *
+  public static function create($params) {
+    $className = 'CRM_Appearancemodifier_DAO_AppearancemodifierPetition';
+    $entityName = 'AppearancemodifierPetition';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    $instance = new $className();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  } */
+
+}
---
+++ Civi/Api4/AppearancemodifierPetition.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * AppearancemodifierPetition entity.
+ *
+ * Provided by the FIXME extension.
+ *
+ * @package Civi\Api4
+ */
+class AppearancemodifierPetition extends Generic\DAOEntity {
+
+}
