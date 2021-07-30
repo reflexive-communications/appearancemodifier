ac61e5dffe64270c8afeea4b1228fb5227c923a3
akosgarai <szlem01@gmail.com>
2021-07-30 17:36:22 +0200

    Implement event admin form backend

--- CRM/Appearancemodifier/Form/Event.php
+++ CRM/Appearancemodifier/Form/Event.php
@@ -1,6 +1,8 @@
 <?php
 
 use CRM_Appearancemodifier_ExtensionUtil as E;
+use Civi\Api4\Event;
+use Civi\Api4\AppearancemodifierEvent;
 
 /**
  * Form controller class
@@ -9,74 +11,136 @@ use CRM_Appearancemodifier_ExtensionUtil as E;
  */
 class CRM_Appearancemodifier_Form_Event extends CRM_Core_Form
 {
-    public function buildQuickForm()
+    private const EVENT_FIELDS = [
+        'layout_handler',
+        'background_color',
+        'outro',
+        'invert_consent_fields',
+        'custom_social_box',
+        'external_share_url',
+        'hide_form_labels',
+    ];
+    // The event, for display some stuff about it on the frontend.
+    private $event;
+
+    /**
+     * Preprocess form
+     *
+     * @throws CRM_Core_Exception
+     */
+    public function preProcess()
     {
-
-    // add form elements
-        $this->add(
-            'select', // field type
-      'favorite_color', // field name
-      'Favorite Color', // field label
-      $this->getColorOptions(), // list of options
-      true // is required
-        );
-        $this->addButtons(array(
-      array(
-        'type' => 'submit',
-        'name' => E::ts('Submit'),
-        'isDefault' => true,
-      ),
-    ));
-
-        // export form elements
-        $this->assign('elementNames', $this->getRenderableElementNames());
-        parent::buildQuickForm();
-    }
-
-    public function postProcess()
-    {
-        $values = $this->exportValues();
-        $options = $this->getColorOptions();
-        CRM_Core_Session::setStatus(E::ts('You picked color "%1"', array(
-      1 => $options[$values['favorite_color']],
-    )));
-        parent::postProcess();
-    }
-
-    public function getColorOptions()
-    {
-        $options = array(
-      '' => E::ts('- select -'),
-      '#f00' => E::ts('Red'),
-      '#0f0' => E::ts('Green'),
-      '#00f' => E::ts('Blue'),
-      '#f0f' => E::ts('Purple'),
-    );
-        foreach (array('1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e') as $f) {
-            $options["#{$f}{$f}{$f}"] = E::ts('Grey (%1)', array(1 => $f));
+        // Get the event id query parameter.
+        $eventId = CRM_Utils_Request::retrieve('eid', 'Integer');
+        // validate event id.
+        $this->event = $this->getEvent($eventId);
+        if ($this->event === []) {
+            throw new CRM_Core_Exception(ts('The selected event seems to be deleted. Id: %1', [1=>$eventId]));
         }
-        return $options;
     }
 
     /**
-     * Get the fields/elements defined in this form.
+     * Set default values
      *
-     * @return array (string)
+     * @return array
      */
-    public function getRenderableElementNames()
+    public function setDefaultValues()
     {
-        // The _elements list includes some items which should not be
-        // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
-        // items don't have labels.  We'll identify renderable by filtering on
-        // the 'label'.
-        $elementNames = array();
-        foreach ($this->_elements as $element) {
-            /** @var HTML_QuickForm_Element $element */
-            $label = $element->getLabel();
-            if (!empty($label)) {
-                $elementNames[] = $element->getName();
-            }
+        $modifiedEvent = AppearancemodifierEvent::get()
+            ->addWhere('event_id', '=', $this->event['id'])
+            ->setLimit(1)
+            ->execute()
+            ->first();
+        // Set defaults
+        foreach (self::EVENT_FIELDS as $key) {
+            $this->_defaults[$key] = $modifiedEvent[$key];
         }
-        return $elementNames;
+        if ($modifiedEvent['background_color'] == null) {
+            $this->_defaults['original_color'] = 1;
+        }
+        return $this->_defaults;
+    }
+
+    /**
+     * Build form
+     */
+    public function buildQuickForm()
+    {
+        $layoutOptions = [
+        ];
+        // Fire hook event.
+        Civi::dispatcher()->dispatch(
+            "hook_civicrm_appearancemodifierEventLayoutHandlers",
+            Civi\Core\Event\GenericHookEvent::create([
+                "options" => &$layoutOptions,
+            ])
+        );
+        $this->add('select', 'layout_handler', ts('Form Layout'), array_merge([''=>ts('Default')], $layoutOptions), false);
+        $this->add('color', 'background_color', ts('Background Color'), [], false);
+        $this->add('wysiwyg', 'outro', ts('Outro Text'), [], false);
+        $this->add('checkbox', 'invert_consent_fields', ts('Invert Consent Fields'), [], false);
+        $this->add('checkbox', 'original_color', ts('Original Background Color'), [], false);
+        $this->add('checkbox', 'hide_form_labels', ts('Hide text input labels'), [], false);
+        $this->add('checkbox', 'custom_social_box', ts('Custom social box'), [], false);
+        $this->add('text', 'external_share_url', ts('External url to share'), [], false);
+        // Submit button
+        $this->addButtons(
+            [
+                [
+                    'type' => 'done',
+                    'name' => ts('Save'),
+                    'isDefault' => true,
+                ],
+                [
+                    'type' => 'cancel',
+                    'name' => ts('Cancel'),
+                ],
+            ]
+        );
+        $this->setTitle(ts('Customize %1 event.', [1=>$this->event['title']]));
+        parent::buildQuickForm();
+    }
+
+    /**
+     * Process post data
+     */
+    public function postProcess()
+    {
+        $submitData = [];
+        foreach (self::EVENT_FIELDS as $key) {
+            $submitData[$key] = $this->_submitValues[$key];
+        }
+        if ($this->_submitValues['original_color'] === '1') {
+            $submitData['background_color'] = '';
+        }
+        $modifiedEvent = AppearancemodifierEvent::update()
+            ->setLimit(1)
+            ->addWhere('event_id', '=', $this->event['id']);
+        foreach (self::EVENT_FIELDS as $key) {
+            $modifiedEvent = $modifiedEvent->addValue($key, $submitData[$key]);
+        }
+        $modifiedEvent = $modifiedEvent->execute();
+        CRM_Core_Session::setStatus(ts('Data has been updated.'), 'Appearancemodifier', 'success', ['expires' => 5000,]);
+
+        parent::postProcess();
+    }
+
+    /*
+     * This function is a wrapper for Event.Get API call.
+     *
+     * @param int $id the event id.
+     *
+     * @return array the result event or empty array.
+     */
+    private function getEvent(int $id): array
+    {
+        $event = Event::get()
+            ->addWhere('id', '=', $id)
+            ->setLimit(1)
+            ->execute();
+        if (count($event) === 0) {
+            return [];
+        }
+        return $event->first();
     }
 }
