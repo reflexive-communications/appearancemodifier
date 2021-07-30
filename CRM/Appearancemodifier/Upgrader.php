ec4400f61a37c35cd2a93dea3fb0ee31d4c0d0b5
akosgarai <szlem01@gmail.com>
2021-07-30 12:33:13 +0200

    Run php-cs-fixer tool on the codebase

--- CRM/Appearancemodifier/Upgrader.php
+++ CRM/Appearancemodifier/Upgrader.php
@@ -4,7 +4,8 @@ use CRM_Appearancemodifier_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Appearancemodifier_Upgrader extends CRM_Appearancemodifier_Upgrader_Base {
+class CRM_Appearancemodifier_Upgrader extends CRM_Appearancemodifier_Upgrader_Base
+{
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
@@ -131,5 +132,4 @@ class CRM_Appearancemodifier_Upgrader extends CRM_Appearancemodifier_Upgrader_Ba
   //   }
   //   return TRUE;
   // }
-
 }
