47cbc9a4a2487899a5fc48fe9dd74b29d92919b6
akosgarai <szlem01@gmail.com>
2021-08-02 16:07:06 +0200

    Add service function for handling the layout changes

--- CRM/Appearancemodifier/Service.php
+++ CRM/Appearancemodifier/Service.php
@@ -21,6 +21,19 @@ class CRM_Appearancemodifier_Service
         'CRM/Event/Form/Registration/Confirm.tpl' => 'CRM/Appearancemodifier/Event/confirm.tpl',
         'CRM/Event/Form/Registration/ThankYou.tpl' => 'CRM/Appearancemodifier/Event/thankyou.tpl',
     ];
+    const PROFILE_TEMPLATES = [
+        'CRM/Appearancemodifier/Profile/edit.tpl',
+    ];
+    const PETITION_TEMPLATES = [
+        'CRM/Appearancemodifier/Petition/signature.tpl',
+        'CRM/Appearancemodifier/Petition/thankyou.tpl',
+    ];
+    const EVENT_TEMPLATES = [
+        'CRM/Appearancemodifier/Event/info.tpl',
+        'CRM/Appearancemodifier/Event/register.tpl',
+        'CRM/Appearancemodifier/Event/confirm.tpl',
+        'CRM/Appearancemodifier/Event/thankyou.tpl',
+    ];
 
     /*
      * This function updates the template name on the profile, petition, event
@@ -231,6 +244,22 @@ class CRM_Appearancemodifier_Service
         }
     }
 
+    public static function alterContent(&$content, $tplName, &$object): void
+    {
+        if (array_search($tplName, self::PROFILE_TEMPLATES) !== false) {
+            self::alterProfileContent($object->getVar('_gid'), $content);
+            return;
+        }
+        if (array_search($tplName, self::PETITION_TEMPLATES) !== false) {
+            self::alterPetitionContent($tplName, $content, $object);
+            return;
+        }
+        if (array_search($tplName, self::EVENT_TEMPLATES) !== false) {
+            self::alterEventContent($tplName, $content, $object);
+            return;
+        }
+    }
+
     /*
      * This function updates the consent fields of the contact.
      *
@@ -250,5 +279,147 @@ class CRM_Appearancemodifier_Service
         if (count($contactData) > 0) {
             CRM_RcBase_Api_Update::contact($contactId, $contactData, false);
         }
+    }
+
+    /*
+     * This function handles the layout changes for the profiles.
+     * If the layout is specified in the modified profile config,
+     * it is applied here.
+     *
+     * @param int $ufGroupId
+     * @param string $content
+     */
+    private static function alterProfileContent($ufGroupId, &$content): void
+    {
+        $modifiedProfile = AppearancemodifierProfile::get(false)
+            ->addWhere('uf_group_id', '=', $ufGroupId)
+            ->execute()
+            ->first();
+        if ($modifiedProfile['layout_handler'] !== null) {
+            $handler = new $modifiedProfile['layout_handler']();
+            $handler->alterContent($content);
+        }
+    }
+
+    /*
+     * This function handles the layout changes for the petitions.
+     * If the layout is specified in the modified petition config,
+     * it is applied here. If the petition message is given, it is
+     * added to the first textarea field on the activity profile.
+     * If the custom social block flag is set, it is updated here.
+     *
+     * @param string $tplName
+     * @param string $content
+     * @param $object
+     */
+    private static function alterPetitionContent($tplName, &$content, &$object): void
+    {
+        // Get the survey id. If we are on the form, it is in the _surveyId variable,
+        // on the thankyou page it is inside the petition array.
+        $id = null;
+        if ($tplName === self::PETITION_TEMPLATES[0]) {
+            $id = $object->getVar('_surveyId');
+        } elseif ($tplName === self::PETITION_TEMPLATES[1]) {
+            $id = $object->getVar('petition')['id'];
+        }
+        // if the id is not found, do nothing.
+        if (is_null($id)) {
+            return;
+        }
+        // Apply the changes that is provided by the layout extension.
+        $modifiedPetition = AppearancemodifierPetition::get(false)
+            ->addWhere('survey_id', '=', $id)
+            ->execute()
+            ->first();
+        if ($modifiedPetition['layout_handler'] !== null) {
+            $handler = new $modifiedPetition['layout_handler']();
+            $handler->alterContent($content);
+        }
+        // If the petition message is set, add it to the relevant field.
+        if ($modifiedPetition['petition_message'] !== null) {
+            $doc = phpQuery::newDocument($content);
+            if ($doc['.crm-petition-activity-profile']->size() > 0) {
+                $doc['.crm-petition-activity-profile textarea:first']->val(new DOMText($modifiedPetition['petition_message']));
+            }
+            $content = $doc->htmlOuter();
+        }
+        // Handle the social block.
+        if ($modifiedPetition['custom_social_box'] !== null) {
+            self::customSocialBlock($content, $modifiedPetition['external_share_url']);
+        }
+    }
+
+    /*
+     * This function handles the layout changes for the events.
+     * If the layout is specified in the modified event config,
+     * it is applied here. If the custom social block flag is set
+     * it is updated also.
+     *
+     * @param string $tplName
+     * @param string $content
+     * @param $object
+     */
+    private static function alterEventContent($tplName, &$content, &$object): void
+    {
+        $id = null;
+        if ($tplName === self::EVENT_TEMPLATES[0]) {
+            $id = $object->getVar('_id');
+        } else {
+            $id = $object->getVar('_eventId');
+        }
+        // if the id is not found, do nothing.
+        if (is_null($id)) {
+            return;
+        }
+        $modifiedEvent = AppearancemodifierEvent::get(false)
+            ->addWhere('event_id', '=', $id)
+            ->execute()
+            ->first();
+        if ($modifiedEvent['layout_handler'] !== null) {
+            $handler = new $modifiedEvent['layout_handler']();
+            $handler->alterContent($content);
+        }
+        // Handle the social block.
+        if ($modifiedPetition['custom_social_box'] !== null) {
+            self::customSocialBlock($content, $modifiedPetition['external_share_url']);
+        }
+    }
+
+    /*
+     * This function handles the layout changes of the social block.
+     *
+     * @param string $content
+     * @param mixed $extenralUrl
+     */
+    private static function customSocialBlock(string &$content, $extenralUrl): void
+    {
+        $doc = phpQuery::newDocument($content);
+        // Update the social block with custom layout.
+        // If not exists, nothing to do with it.
+        if ($doc['.crm-socialnetwork']->size() > 0) {
+            $twitter = '';
+            $facebook = '';
+            // Build the new icons based on the data of the original buttons.
+            // The onclick event is reused as the click handler of the new social links.
+            foreach ($doc['.crm-socialnetwork button'] as $button) {
+                switch ($button->getAttribute('id')) {
+                case 'crm-tw':
+                    $twitter = '<div class="social-media-icon"><a href="#" onclick="'.$button->getAttribute('onclick').'" target="_blank" title="'.E::ts('Share on Twitter').'"><div><i aria-hidden="true" class="crm-i fa-twitter"></i></div></a></div>';
+                    break;
+                case 'crm-fb':
+                    $facebook = '<div class="social-media-icon"><a href="#" onclick="'.$button->getAttribute('onclick').'" target="_blank" title="'.E::ts('Share on Facebook').'"><div><i aria-hidden="true" class="crm-i fa-facebook"></i></div></a></div>';
+                    break;
+                }
+            }
+            // Make the update only if the parsing process was successful.
+            if ($twitter !== '' || $facebook !== '') {
+                // The original block has to be deleted as it is unused.
+                $doc['.crm-socialnetwork']->remove();
+                // Build the block and append it to the main content.
+                $socialTemplate = '<div class="crm-section crm-socialnetwork"><h2>'.E::ts('Please share it').'</h2><div class="appearancemodifier-social-block">'.$facebook.$twitter.'</div></div>';
+                $doc['#crm-main-content-wrapper']->append(phpQuery::newDocument($socialTemplate));
+            }
+        }
+        $content = $doc->htmlOuter();
     }
 }
