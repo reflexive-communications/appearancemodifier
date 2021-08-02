2c66b4a8451d9ce46c72ce1a7efb825b9add5d67
akosgarai <szlem01@gmail.com>
2021-08-02 21:13:22 +0200

    Add testfile for AbstractLayout class

---
+++ tests/phpunit/CRM/Appearancemodifier/AbstractLayoutTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Testcase for the AbstractLayout
+ * This is a generic test class for the extension (implemented with PHPUnit).
+ */
+class LayoutImplementation extends CRM_Appearancemodifier_AbstractLayout
+{
+    public function setStyleSheets(): void
+    {
+    }
+    public function alterContent(string &$content): void
+    {
+    }
+}
+class CRM_Appearancemodifier_AbstractLayoutTest extends \PHPUnit\Framework\TestCase
+{
+
+  /**
+   * The setup() method is executed before the test is executed (optional).
+   */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * The tearDown() method is executed after the test was executed (optional)
+     * This can be used for cleanup.
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test for className.
+     */
+    public function testClassName()
+    {
+        $impl = new LayoutImplementation();
+        self::assertSame('appearancemodifier-', $impl->className());
+    }
+}
