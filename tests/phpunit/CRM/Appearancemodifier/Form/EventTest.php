a735e19fa74f4ab096c6a9370d5bc2cd1f3fbf63
akosgarai <szlem01@gmail.com>
2021-08-02 21:23:54 +0200

    Add testfile for Event admin form

---
+++ tests/phpunit/CRM/Appearancemodifier/Form/EventTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This is a generic test class for the extension (implemented with PHPUnit).
+ */
+class CRM_Appearancemodifier_Form_EventTest extends \PHPUnit\Framework\TestCase
+{
+    public function setUpHeadless()
+    {
+        return \Civi\Test::headless()
+            ->install('rc-base')
+            ->installMe(__DIR__)
+            ->apply();
+    }
+
+    /**
+     * Apply a forced rebuild of DB, thus
+     * create a clean DB before running tests
+     *
+     * @throws \CRM_Extension_Exception_ParseException
+     */
+    public static function setUpBeforeClass(): void
+    {
+        // Resets DB and install depended extension
+        \Civi\Test::headless()
+            ->install('rc-base')
+            ->installMe(__DIR__)
+            ->apply(true);
+    }
+
+    /**
+     * Create a clean DB after running tests
+     *
+     * @throws CRM_Extension_Exception_ParseException
+     */
+    public static function tearDownAfterClass(): void
+    {
+        \Civi\Test::headless()
+            ->uninstallMe(__DIR__)
+            ->uninstall('rc-base')
+            ->apply(true);
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /*
+     * It tests the preProcess function.
+     */
+    public function testPreProcess()
+    {
+        self::markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    /*
+     * It tests the setDefaultValues function.
+     */
+    public function testSetDefaultValues()
+    {
+        self::markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    /*
+     * It tests the buildQuickForm function.
+     */
+    public function testBuildQuickForm()
+    {
+        self::markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    /*
+     * It tests the postProcess function.
+     */
+    public function testPostProcess()
+    {
+        self::markTestIncomplete('This test has not been implemented yet.');
+    }
+}
