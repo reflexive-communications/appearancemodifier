d1cce0698089247a1c25b739a6be02926d0e7505
akosgarai <szlem01@gmail.com>
2021-11-19 15:45:24 +0100

    Extend service testcase - cover the consentactivity process

--- tests/phpunit/CRM/Appearancemodifier/ServiceConsentTest.php
+++ tests/phpunit/CRM/Appearancemodifier/ServiceConsentTest.php
@@ -207,4 +207,33 @@ class CRM_Appearancemodifier_ServiceConsentTest extends CRM_Appearancemodifier_F
             ->execute();
         self::assertCount(count($activityContactsBefore) + 1, $activityContactsAfter);
     }
+    public function testAlterContentConsentCheckboxes()
+    {
+        $customField = parent::createNewCustomField();
+        // setup conset activity configuration
+        $config = new CRM_Consentactivity_Config('consentactivity');
+        $config->load();
+        $cfg = $config->get();
+        $cfg['custom-field-map'][] = [
+            'custom-field-id' => 'custom_'.$customField['id'],
+            'consent-field-id' => 'do_not_phone',
+            'group-id' => '0',
+        ];
+        $config->update($cfg);
+        $profile = UFGroup::create(false)
+            ->addValue('title', 'Test UFGroup aka Profile')
+            ->addValue('is_active', true)
+            ->execute()
+            ->first();
+        UFField::create(false)
+            ->addValue('uf_group_id', $profile['id'])
+            ->addValue('field_name', 'custom_'.$customField['id'])
+            ->execute();
+        $form = new CRM_Profile_Form_Edit();
+        $form->setVar('_gid', $profile['id']);
+        $expectedContent = "<div><div class=\"crm-section form-item consentactivity\" id=\"editrow-custom_".$customField['id']."\">\n<div class=\"label\"><label>Move me.</label></div>\n<div class=\"content\"><input type=\"checkbox\" id=\"custom_".$customField['id']."_1\"></div>\n</div></div>";
+        $content = '<div><div class="crm-section form-item" id="editrow-custom_'.$customField['id'].'"><div class="label"><label>Replace me.</label></div><div class="content"><input type="checkbox" id="custom_'.$customField['id'].'_1" /><label>Move me.</label></div></div></div>';
+        self::assertEmpty(CRM_Appearancemodifier_Service::alterContent($content, CRM_Appearancemodifier_Service::PROFILE_TEMPLATES[0], $form));
+        self::assertSame($expectedContent, $content, 'Invalid content has been generated template: '.CRM_Appearancemodifier_Service::PROFILE_TEMPLATES[0].'. '.$content);
+    }
 }
