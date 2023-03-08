953c84fa224b4da2dcb76a5b2ce24f89835bb657
Sandor Semsey <sandor@es-progress.hu>
2023-03-09 00:25:58 +0100

    reformat js files

--- assets/js/checkallcheckbox.js
+++ assets/js/checkallcheckbox.js
@@ -1,6 +1,7 @@
 /*
  * This application provides the onclick handler of the check all checkboxes on the form feature.
  */
+
 // This function sets the checked properties of the checkboxes on the form
 function checkAllCheckboxClickHandler(item) {
     let items = document.querySelectorAll('input[type="checkbox"]');
@@ -8,6 +9,7 @@ function checkAllCheckboxClickHandler(item) {
         items[i].checked = item.checked;
     }
 }
+
 /*
  * Click event callback for the rest of the checkboxes.
  * When the item is unchecked, the check all checkbox has to be also unchecked.
@@ -18,6 +20,7 @@ function uncheckCheckAllCheckbox(event) {
     }
     document.querySelector('#check-all-checkbox-item').checked = false;
 }
+
 /*
  * Attach the event handlers to the checkboxes.
  * */
