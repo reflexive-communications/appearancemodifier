efea9a32e2732d0f64f05b1f404dc257e845ce7b
Sandor Semsey <sandor@es-progress.hu>
2025-01-15 14:57:16 +0100

    use Entity Framework v2

--- xml/schema/CRM/Appearancemodifier/AppearancemodifierPetition.entityType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    [
-        'name' => 'AppearancemodifierPetition',
-        'class' => 'CRM_Appearancemodifier_DAO_AppearancemodifierPetition',
-        'table' => 'civicrm_appearancemodifier_petition',
-    ],
-];
